d89de94efa32d822c0841450ef17fb3f55c7106d
iankala <iankala12814@gmail.com>
2026-01-30 09:40:56 +0300

    Convert frontend from submodule to regular directory

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react"
+import { useWorkoutsContext} from '../hooks/useWorkoutsContext'
+
+//components
+import WorkoutForm from "../components/workoutForm"
+import WorkoutDetails from '../components/WorkoutDetails'
+
+
+const Home = ()=>{
+    const {workouts, dispatch} = useWorkoutsContext()
+
+
+    useEffect(()=> {
+        const fetchWorkouts = async () =>{
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok){
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+
+        }
+
+        fetchWorkouts()
+
+    }, [dispatch])
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout)=>(
+                  <WorkoutDetails key = {workout._id} workout={workout}/>
+
+                ))}
+
+            </div>
+                <WorkoutForm />
+
+        </div>
+
+    )
+
+}
+
+export default Home
