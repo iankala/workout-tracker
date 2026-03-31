dfa725a7f49923972fe811ee8d822e784dbaf1d4
Ian Kala <iankala12814@gmail.com>
2026-03-31 16:30:29 +0300

    Update user model and frontend styling

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,51 +1,48 @@
-import { useEffect } from "react"
-import { useAuthContext} from "../hooks/useAuthContext"
-import { useWorkoutsContext} from '../hooks/useWorkoutsContext'
+import { useEffect } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 //components
-import WorkoutForm from "../components/workoutForm"
-import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from "../components/workoutForm";
+import WorkoutDetails from "../components/WorkoutDetails";
 
+const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
-const Home = ()=>{
-    const {workouts, dispatch} = useWorkoutsContext()
-    const {user} = useAuthContext()
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      const json = await response.json();
 
-    useEffect(()=> {
-        const fetchWorkouts = async () =>{
-            const response = await fetch('/api/workouts', {
-                headers: {
-                    'Authorization': `Bearer ${user.token}`
-                }
-            })
-            const json = await response.json()
+      if (response.ok) {
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
 
-            if (response.ok){
-                dispatch({type: 'SET_WORKOUTS', payload: json})
-            }
+    if (user) {
+      fetchWorkouts();
+    }
+  }, [dispatch, user]);
 
-        }
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.length === 0 && (
+          <p>No workouts yet. Add your first one!</p>
+        )}
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
 
-        if(user){
-            fetchWorkouts()
-        }
-    }, [dispatch, user])
-
-    return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout)=>(
-                  <WorkoutDetails key = {workout._id} workout={workout}/>
-
-                ))}
-
-            </div>
-                <WorkoutForm />
-
-        </div>
-
-    )
-
-}
-
-export default Home
+export default Home;
